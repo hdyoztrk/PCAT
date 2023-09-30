877830e96bb76b49f6b5b2b2e6f74eb312698209
hdyoztrk <hediyeoztr@gmail.com>
2023-09-30 14:56:53 +0300

    files uploaded

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const ejs = require("ejs")
+const path = require("path")
+
+const app = express();
+
+//template engine
+app.set("view engine", "ejs")
+
+//mıddlewares
+app.use(express.static("public"))
+
+//routes
+app.get('/', (req, res) => {
+    res.render("index")
+})
+app.get('/about', (req, res) => {
+    res.render("about")
+})
+app.get('/add', (req, res) => {
+    res.render("add")
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`sunucu ${port} portunda başlatıldı..`)
+});
